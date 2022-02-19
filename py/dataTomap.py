269a593b9462074824bce18e46efece10da74b6f
511064363 <511064363@QQ.COM>
2022-02-19 23:17:26 +0800

    path

--- py/dataTomap.py
+++ py/dataTomap.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-(a,b,c,d,e,f,g,h,i)=np.loadtxt("~\sd.txt",dtype=int,skiprows=1,comments='#',delimiter=',',usecols=(0,1,2,3,4,5,6,7,8),unpack=True)
+(a,b,c,d,e,f,g,h,i)=np.loadtxt("c:/sd.txt",dtype=int,skiprows=1,comments='#',delimiter=',',usecols=(0,1,2,3,4,5,6,7,8),unpack=True)
 plt.title('800_8')
 plt.plot(a-25,color='green',label='adc',alpha=0.50)
 plt.plot(c-80,color='blue',label='adm',alpha=0.50)
