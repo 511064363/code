51556377e2edf0fd1ef200176ba65a06a71682b9
511064363 <511064363@QQ.COM>
2023-03-12 21:45:24 +0800

    Create t2p.py

--- py/t2p.py
+++ py/t2p.py
@@ -4,7 +4,7 @@ import os
 import datetime
 import numpy as np
 import matplotlib.pyplot as plt
-
+apa=1
 __time = datetime.datetime.now().strftime('%g%m%d%H')
 
 path = os.getcwd()  # 获取当前路径
@@ -23,15 +23,15 @@ for j in listDir:
         plt.rcParams['font.sans-serif'] = ['SimHei']
         plt.rcParams['axes.unicode_minus'] = False
         plt.title(os.path.splitext(j)[0]+'_'+__time,fontsize=30)
-        plt.plot(b*bx-by+np.ptp(c)+60, color='green', label='adc', alpha=0.5, linewidth=0.8)
-        plt.plot(c, color='blue', label='adm', alpha=0.5, linewidth=0.8)
-        plt.plot(d, color='orange', label='base', alpha=0.5, linewidth=0.8)
-        plt.plot(a*ax+ay, color='red', label='key', alpha=0.5, linewidth=0.8)
+        plt.plot(b*bx-by+np.ptp(c)+60, color='green', label='adc', alpha=apa, linewidth=0.8)
+        plt.plot(c, color='blue', label='adm', alpha=apa, linewidth=0.8)
+        plt.plot(d, color='orange', label='base', alpha=apa, linewidth=0.8)
+        plt.plot(a*ax+ay, color='red', label='key', alpha=apa, linewidth=0.8)
 
         plt.xlim(-10, len(a) + 26)
-        plt.legend()
+        plt.legend(frameon=False)
         plt.draw()
         plt.gcf().set_size_inches(50, 8)
+        plt.savefig(str(z) + '_' + os.path.splitext(j)[0] + ".png",transparent=True,dpi=200,bbox_inches="tight")
         plt.show()
-        plt.savefig(str(z) + '_' + os.path.splitext(j)[0] + ".png", dpi=200)
         plt.close()
