74f4d1c5ec1e4480c049968792d37e14b6df486c
511064363 <511064363@QQ.COM>
2024-03-14 19:07:17 +0800

    Update Log-Analysis.py

--- py/Log-Analysis.py
+++ py/Log-Analysis.py
@@ -6,7 +6,7 @@ log_files = glob.glob(current_path + '/**/*.log', recursive=True)
 log_files.sort()    #排序
 
 output_file = 'output.txt'
-keywords = ['PSR_Arm4:Y','Cnl Magnet Detector Pressed','Behavior Task/Match Grip(17): Task Started Active','Cnl Magnet Detector Released','Enter Power Off Countdown','is dead deteacted by AppKivelinesMonitor in']
+keywords = ['PSR_Arm4:Y','Cnl Magnet Detector Pressed','Behavior Task/Match Grip(17): Task Started Active','Cnl Magnet Detector Released','Enter Power Off Countdown','is dead detected by AppLivelinessMonitor in']
 
 def find_lines_with_keywords(input_file, output_file, keywords):
     with open(input_file, 'r') as infile, open(output_file, 'a') as outfile:
